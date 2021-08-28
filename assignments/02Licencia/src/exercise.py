28ae1df7125003909686875357ab61e03afefd13
Angel Perez <a00833469@itesm.mx>
2021-08-28 11:10:01 -0500

    Tarea condiconales basicos

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -2,6 +2,19 @@
 def main():
     edad = int(input("Ingresa tu edad: "))
     #Aquí empieza tu programa...
+    if edad >= 18:
+       identi = str(input("¿Tienes identificación oficial? (s/n): "))
+       if identi == "s":
+         print("Trámite de licencia concedido")
+       elif identi == "n":
+         print("No cumples requisitos")
+       else:
+        print("Respuesta incorrecta")
+    
+    elif edad <18 and edad > 0:
+      print("No cumples requisitos")
+    else:
+      print("Respuesta incorrecta")
 
 if __name__ == '__main__':
     main()
--- assignments/03TipoTriangulo/src/exercise.py
+++ assignments/03TipoTriangulo/src/exercise.py
@@ -4,6 +4,18 @@ def main():
     lado3 = int(input("Ingresa la medida del lado 3: "))
     #Escribe aquí tu código...
 
+    if lado2 + lado1 > lado3 and lado3 + lado1 > lado2 and lado3 + lado2 > lado1:
+        if lado2 == lado3 and lado1 == lado3 and lado1 == lado2 :
+            print("ES UN TRIANGULO EQUILATERO")
+        elif lado1 != lado2 and lado1 != lado3 and lado2 != lado3 :
+            print("ES UN TRIANGULO ESCALENO")
+        elif lado1 == lado2 or lado1 == lado3 or lado3 == lado2:
+            print("ES UN TRIANGULO ISOSCELES")
+    else:
+        print("NO ES TRIANGULO")
+
+
+
 
 if __name__=='__main__':
     main()
